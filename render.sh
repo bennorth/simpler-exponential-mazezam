871980118388b234c64349482a7935c389208859
Ben North <ben@redfrontdoor.org>
2017-08-14 22:34:54 +0100

    Refresh browser after render

--- render.sh
+++ render.sh
@@ -2,6 +2,8 @@
 
 # Written by Ben North, 2017.
 # Hereby placed into the public domain.
+# (Requires local 'refresh-chrome' utility.)
 
 python aw-png-from-txt.py < aw-6-bar.txt
 pandoc content.md -c hugo-octopress.css -H extra-head.html -o dist/index.html
+refresh-chrome "Mazezam level family"
