9b7bd57fd267481fb22fffff48cc00ee7347192e
Ben North <ben@redfrontdoor.org>
2017-08-27 10:30:33 +0100

    Put nops back into shove-row steps

--- game.js
+++ game.js
@@ -143,8 +143,6 @@ jQuery(document).ready(function($)
             p.counter.sync_display(gray_is_valid);
         };
 
-        return [move_and_adjust];
-
         var nops = repeated(2, function(sched) { sched.pause(1); });
         return nops.concat([move_and_adjust]).concat(nops);
     }
